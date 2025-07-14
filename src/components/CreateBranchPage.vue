b220ef99ea8dcab8f393f211c6f059475843d088
AdithyaPadmashali <adithya.p2001@gmail.com>
2025-07-15 03:36:43 +0530

    Add create branch page TODO: add a random button in story view

--- src/components/CreateBranchPage.vue
+++ src/components/CreateBranchPage.vue
@@ -1,5 +1,36 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+import { useStoryStore } from '../stores/story';
+import axios from 'axios';
+
+const storyStore = useStoryStore();
+
+const GET_BRANCHABLE_STORIES_URL = `http://localhost:8000/api/v1/branch-req/${storyStore.story.id}/branchables`
+
+const available_stories = ref([])
+
+const getBranchableStories = () => {
+    axios.get(GET_BRANCHABLE_STORIES_URL)
+        .then(res => {
+            available_stories.value = res.data
+        })
+        .catch(err => {
+            console.error("Something went wrong ", err)
+        })
+}
+
+onMounted(() => {
+    getBranchableStories()
+})
+
+</script>
 <template>
-    Branch this story with
-    //I need to get all the possible stories that I can branch with
-    // stories whose Ids are not present in branch table src ids list
+    <div class="create-branch-container">
+        {{ storyStore.story.title }} ->
+        <select id="create-branch-container" name="available_branches">
+            <option v-for="story in available_stories" :key="story.id" :value="story.id">
+                {{ story.title }}
+            </option>
+        </select>
+    </div>
 </template>
